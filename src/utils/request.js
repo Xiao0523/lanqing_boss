b514e3d31301a38957e34a14465b1cf16d1289d4
chaiyuzhe <785487784@qq.com>
2020-03-12 17:01:33 +0800

    [TEST] 测试接口请求

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,9 +7,10 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  // headers: {
-  //   'X-Requested-With': 'XMLHttpRequest'
-  // },
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+    'Content-Type': 'application/json;charset=utf-8'
+  },
   // baseURL: Api_url, // process.env.VUE_APP_BASE_API, // url = base url + request url
   withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout\
@@ -26,13 +27,14 @@ service.interceptors.request.use(
     } else {
       config.baseURL = Api_url
     }
+    config.headers['common']['Content-Type'] = 'application/x-www-form-urlencoded'
     // do something before request is sent\
-    if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers['token'] = getToken()
-    }
+    // if (store.getters.token) {
+    //   // let each request carry token
+    //   // ['X-Token'] is a custom headers key
+    //   // please modify it according to the actual situation
+    //   config.headers['token'] = getToken()
+    // }
     return config
   },
   error => {
