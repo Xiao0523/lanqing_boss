12ba3cde5aee43ce4700d1ac9d68592ced63b137
chaiyuzhe <785487784@qq.com>
2020-03-18 09:36:12 +0800

    [FIX] 修改了新的UI

--- src/utils/request.js
+++ src/utils/request.js
@@ -30,14 +30,13 @@ service.interceptors.request.use(
       config.baseURL = Api_url
     }
     config.headers['common']['Content-Type'] = 'application/x-www-form-urlencoded'
-    config.headers['get']['Content-Type'] = 'application/x-www-form-urlencoded'
     // do something before request is sent\
-    if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers['token'] = getToken()
-    }
+    // if (store.getters.token) {
+    //   // let each request carry token
+    //   // ['X-Token'] is a custom headers key
+    //   // please modify it according to the actual situation
+    //   config.headers['token'] = getToken()
+    // }
     // console.log(store.getters.token)
     return config
   },
@@ -63,7 +62,6 @@ service.interceptors.response.use(
   response => {
     const res = response.data
     // if the custom code is not 20000, it is judged as an error.
-    console.log(document.cookie)
     return res
   },
   error => {
