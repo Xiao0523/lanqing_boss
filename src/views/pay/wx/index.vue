65cf19de56fedf5c530e314e5289ce5be402ae36
chaiyuzhe <785487784@qq.com>
2020-04-17 14:57:09 +0800

    [Feat] 测试支付功能-二维码

--- src/views/pay/wx/index.vue
+++ src/views/pay/wx/index.vue
@@ -4,6 +4,7 @@
 
 <script>
 import { getConfigList } from '@/api/pay'
+const wx = window.wx
 export default {
   name: 'WxPay',
   data() {
@@ -18,7 +19,14 @@ export default {
         target: window.location.href
       }
       getConfigList(getObj).then(res => {
-        console.log(res)
+        wx.config({
+          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId: res.appId, // 必填，公众号的唯一标识
+          timestamp: res.timestamp, // 必填，生成签名的时间戳
+          nonceStr: res.nonceStr, // 必填，生成签名的随机串
+          signature: res.signature, // 必填，签名
+          jsApiList: [window.location.href] // 必填，需要使用的JS接口列表
+        })
       })
     }
   }
