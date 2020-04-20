217e825f15366b1ea1ba319a2166ce3de7ceb4f6
chaiyuzhe <785487784@qq.com>
2020-04-20 11:02:53 +0800

    [Test] 微信支付功能测试

--- src/views/pay/wx/index.vue
+++ src/views/pay/wx/index.vue
@@ -56,19 +56,19 @@ export default {
                   storeId: getLocal('storeId')
                 }
                 wxPay(payObj).then(res => {
-                  console.log(res)
+                  wx.chooseWXPay({
+                    timestamp: res.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                    nonceStr: res.nonceStr, // 支付签名随机串，不长于 32 位
+                    package: res.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
+                    signType: res.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+                    paySign: res.paySign, // 支付签名
+                    success: function(res) {
+                      console.log(res)
+                      // 支付成功后的回调函数
+                    }
+                  })
                 })
               })
-              // wx.chooseWXPay({
-              //   timestamp: 0, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-              //   nonceStr: '', // 支付签名随机串，不长于 32 位
-              //   package: '', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
-              //   signType: '', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-              //   paySign: '', // 支付签名
-              //   success: function(res) {
-              //     // 支付成功后的回调函数
-              //   }
-              // })
             }
           })
         })
