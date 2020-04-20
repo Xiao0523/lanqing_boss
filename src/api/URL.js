f335558083bdf38f7b44b0585b3384c86d3cfe19
chaiyuzhe <785487784@qq.com>
2020-04-20 15:05:23 +0800

    [Fix] 正式接口

--- src/api/URL.js
+++ src/api/URL.js
@@ -5,7 +5,7 @@ const UPLOAD_BASE_URL = 'https://upload.my51share.com'
 const Upload_Pic = UPLOAD_BASE_URL + '/pic' // 'https://dev.my51share.com/boss/upload' //
 const Upload_Video = UPLOAD_BASE_URL + '/video'
 const Upload_File = UPLOAD_BASE_URL + '/file'
-const Api_url = 'https://lanqing.my51share.com'
+const Api_url = 'https://www.yourlanqing.com/'
 const Web_Api_url = 'https://www.my51share.com'
 const Rongyun_Api = 'https://api-cn.ronghub.com/'
 
