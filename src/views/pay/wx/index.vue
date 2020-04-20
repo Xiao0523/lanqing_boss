7947ad44e232d07b36cdea71b4727f5e9ad59019
chaiyuzhe <785487784@qq.com>
2020-04-20 11:01:01 +0800

    [Test] 微信支付功能测试

--- src/views/pay/wx/index.vue
+++ src/views/pay/wx/index.vue
@@ -50,13 +50,13 @@ export default {
                 code: _this.$route.query.code
               }
               getOpenId(codeObj).then(res => {
-                console.log(getLocal('amount'), getLocal('storeId'))
                 const payObj = {
                   openId: res.openid,
                   amount: getLocal('amount'),
                   storeId: getLocal('storeId')
                 }
                 wxPay(payObj).then(res => {
+                  console.log(res)
                 })
               })
               // wx.chooseWXPay({
