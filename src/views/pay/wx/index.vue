3c488e904fea8c376364d04a1fa58ff390e4f5c3
chaiyuzhe <785487784@qq.com>
2020-04-20 09:33:11 +0800

    [Test] 微信支付功能测试

--- src/views/pay/wx/index.vue
+++ src/views/pay/wx/index.vue
@@ -27,44 +27,42 @@ export default {
           const url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + res.appId + '&redirect_uri=' + href + '&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect'
           location.href = url
           return false
-        } else {
-          console.log(href.indexOf('code'), 2)
-          const codeObj = {
-            code: _this.$route.query.code
-          }
-          getOpenId(codeObj).then(res => {
-            console.log('res:', res)
-          })
         }
-        // wx.config({
-        //   debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-        //   appId: res.appId, // 必填，公众号的唯一标识
-        //   timestamp: res.timestamp, // 必填，生成签名的时间戳
-        //   nonceStr: res.nonceStr, // 必填，生成签名的随机串
-        //   signature: res.signature, // 必填，签名
-        //   jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表
-        // })
-        // wx.ready(function() {
-        //   wx.checkJsApi({
-        //     jsApiList: ['chooseWXPay'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
-        //     success: function(res) {
-        //       // wx.chooseWXPay({
-        //       //   timestamp: 0, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-        //       //   nonceStr: '', // 支付签名随机串，不长于 32 位
-        //       //   package: '', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
-        //       //   signType: '', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-        //       //   paySign: '', // 支付签名
-        //       //   success: function(res) {
-        //       //     // 支付成功后的回调函数
-        //       //   }
-        //       // })
-        //     }
-        //   })
-        // })
-        // wx.error(function(res) {
-        //   // console.log(res)
-        //   // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-        // })
+        wx.config({
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId: res.appId, // 必填，公众号的唯一标识
+          timestamp: res.timestamp, // 必填，生成签名的时间戳
+          nonceStr: res.nonceStr, // 必填，生成签名的随机串
+          signature: res.signature, // 必填，签名
+          jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表
+        })
+        wx.ready(function() {
+          wx.checkJsApi({
+            jsApiList: ['chooseWXPay'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+            success: function(res) {
+              const codeObj = {
+                code: _this.$route.query.code
+              }
+              getOpenId(codeObj).then(res => {
+                console.log('res:', res)
+              })
+              // wx.chooseWXPay({
+              //   timestamp: 0, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+              //   nonceStr: '', // 支付签名随机串，不长于 32 位
+              //   package: '', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
+              //   signType: '', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+              //   paySign: '', // 支付签名
+              //   success: function(res) {
+              //     // 支付成功后的回调函数
+              //   }
+              // })
+            }
+          })
+        })
+        wx.error(function(res) {
+          // console.log(res)
+          // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+        })
         // alert(location.href.split('#')[0])
       })
     }
